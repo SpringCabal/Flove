ccead898575af63bcd6d9a2fda1ef9ebc47cc086
Anarchid <anarchid@mail.ru>
2015-12-12 17:24:36 +0200

    function to show spire branches

--- Scripts/spire.lua
+++ Scripts/spire.lua
@@ -1,3 +1,37 @@
+local Stalk = piece('Stalk');
+local Eye = piece('Eye');
+
+local Leaves={};
+local Branch={};
+
+local function ShowBranch(n)
+	local x,y,z = Spring.GetUnitPiecePosDir(unitID,Branch[n]);
+	Spring.SpawnCEG("dirtfling", x, y, z, 0, 0, 0, 0);
+	Show(Branch[n]);
+	
+	Sleep(500)
+	
+	x,y,z = Spring.GetUnitPiecePosDir(unitID,Leaves[n]);
+	Spring.SpawnCEG("dirtfling", x, y, z, 0, 0, 0, 0);
+	Show(Leaves[n]);
+end
+
+function script.Create()
+	for i=1,9 do
+		Leaves[i] = piece('Leaves'..i);
+		Branch[i] = piece('Branch'..i);
+		Hide(Leaves[i]);
+		Hide(Branch[i]);
+	end
+	
+	Sleep(1000);
+	
+	for i=1,9 do
+		ShowBranch(i);
+		Sleep(1000);
+	end
+end
+
 function script.Killed(recentDamage, maxHealth)
-	return 0
+	return 0;
 end
