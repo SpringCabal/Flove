4a5b75afb91fe6f80d1cd10805bdef4107936d14
silentwings <nicfreeman1209@hotmail.com>
2015-12-12 18:59:14 +0000

    add mana counter display

---
+++ LuaUI/Widgets/ui_mana_display.lua
@@ -0,0 +1,54 @@
+-- WIP
+function widget:GetInfo()
+	return {
+		name    = 'Mana Display',
+		desc    = '',
+		author  = 'Bluestone',
+		date    = 'rawr',
+		license = 'Horses',
+        layer = 0,
+		enabled = true,
+	}
+end
+
+local Chili, Screen0
+local textBox
+local manaColour = "\255\204\0\99"
+local nMana = 0
+
+function resizeUI(vsx,vsy)
+    if quitButton ~= nil then
+        quitButton:SetPos(vsx*0.52, vsy*(1-0.05-0.01), vsx*0.05, vsy*0.05) 
+    end
+end
+
+function widget:ViewResize(vsx,vsy)
+	resizeUI(vsx,vsy)
+end
+
+function widget:Initialize()
+	Chili = WG.Chili
+    Screen0 = Chili.Screen0
+    
+    textBox = Chili.TextBox:New{
+        parent = Screen0,
+        x = 10,
+        y = Screen0.height-175,
+        width = 200,
+        text = "Mana: " .. manaColour .. "0",        
+        font = {
+            size = 20,
+        },
+    }
+        
+    local vsx,vsy = Spring.GetViewGeometry()
+    resizeUI(vsx,vsy)
+end
+
+function widget:GameFrame()
+    local nManaNew = Spring.GetGameRulesParam("mana")
+    if nMana ~= nManaNew then
+        textBox:SetText("Mana: " .. manaColour .. nManaNew)
+        nMana = nManaNew
+    end
+end
