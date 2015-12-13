df6d1ee1af11b989e8c4e042c48e7b45c2634f55
Gajo Petrovic <gajo01@gmail.com>
2015-12-13 19:10:09 +0900

    shake it up!

--- ModelMaterials/1_tree_normalmapping.lua
+++ ModelMaterials/1_tree_normalmapping.lua
@@ -13,7 +13,15 @@ local function DrawUnit(unitID, material)
 	if frameLocID == nil then
 		frameLocID = gl.GetUniformLocation(material.shader, "frameLoc")
 	end
-	local frame = 0.001 * sine(modulo(unitID, 10) + GetGameFrame() / (modulo(unitID, 7) + 6))
+	local unitDefID = Spring.GetUnitDefID(unitID)
+	local unitDef = UnitDefs[unitDefID]
+	local factor = 0.0001
+	if unitDef.customParams.tree then
+		factor = 0.002
+	elseif unitDef.customParams.shrubs then
+		factor = 0.008
+	end
+	local frame = factor * sine(modulo(unitID, 10) + GetGameFrame() / (modulo(unitID, 7) + 6))
 	glUniform(frameLocID, frame)
 --   health,maxhealth=GetUnitHealth(unitID)
 --   glUniform(material.healthLoc, 2*maximum(0, (-2*health)/(maxhealth)+1) )
@@ -51,6 +59,21 @@ local unitMaterials = {
    treelevel1 = "tree",
    treelevel2 = "tree",
    treelevel3 = "tree",
+   
+   grass1 = "tree",
+   grass2 = "tree",
+   grass3 = "tree",
+   grass4 = "tree",
+   grass5 = "tree",
+   
+   flower1 = "tree",
+   flower2 = "tree",
+   flower3 = "tree",
+   flower4 = "tree",
+   flower5 = "tree",
+   flower6 = "tree",
+   
+   spire = "tree",
 }
 
 
