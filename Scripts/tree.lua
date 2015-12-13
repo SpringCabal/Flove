a4568db2d138151449d2bc191660a460c6918056
Anarchid <anarchid@mail.ru>
2015-12-13 19:49:12 +0200

    fix tree piece names and tree death

--- Scripts/tree.lua
+++ Scripts/tree.lua
@@ -1,6 +1,6 @@
 function script.Killed(recentDamage, maxHealth)
 	local x,y,z = Spring.GetUnitPosition(unitID);
-	local trunk = piece('Mesh__3_');
+	local trunk = piece('Trunk');
 	Spring.SpawnCEG("blackpop", x, y, z, 0, 2, 0, 10,10);
 	Turn(trunk,z_axis,2,1);	
 	WaitForTurn(trunk,z_axis);
