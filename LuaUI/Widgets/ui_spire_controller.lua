1ea6c3a5b698f7c4d122b48423524e197c4c82b3
silentwings <nicfreeman1209@hotmail.com>
2015-12-13 14:37:03 +0000

    fix lua error

--- LuaUI/Widgets/ui_spire_controller.lua
+++ LuaUI/Widgets/ui_spire_controller.lua
@@ -38,6 +38,7 @@ function widget:MousePress(mx, my, button)
 	local alt, ctrl, meta, shift = Spring.GetModKeyState()
 	if not Spring.IsAboveMiniMap(mx, my) then
 		local traceType, pos = Spring.TraceScreenRay(mx, my, true)
+        if not pos then return false end
         local x, y, z = pos[1], pos[2], pos[3]
 
 		if x<2048 or z<2048 or x>8192 or z>8192 then	 
