5dfb051bbbd4e8f9dcb4728919e64c339fd84095
Anarchid <anarchid@mail.ru>
2015-12-14 22:52:59 +0200

    completely abolish flower colvols

--- Scripts/flower.lua
+++ Scripts/flower.lua
@@ -1,6 +1,10 @@
 function script.Create()
 	local x,y,z = Spring.GetUnitPosition(unitID);
 	Spring.SpawnCEG("dirtfling", x, y, z, 0, 0, 0, 0);
+	Spring.SetUnitCollisionVolumeData(unitID,0, 0, 0,
+										     0, 0, 0,
+										     -1, 0, 0);
+	
 end
 
 function script.Killed(recentDamage, maxHealth)
